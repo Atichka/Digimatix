68fc0397766ffe3cdf3e64c4b3b2b4c840389697
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-11-02 00:00:54 +0300

    add section offers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./style.css";
+import Swiper from 'swiper';
+
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  fadeEffect: {
+    crossFade: 'true'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.offers__button-next',
+    prevEl: '.offers__button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
